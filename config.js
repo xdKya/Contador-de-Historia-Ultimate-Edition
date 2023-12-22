0f5a9a2d63a3b094f99d12d052cbf39e612dd54f
xdKya <90745676+xdKya@users.noreply.github.com>
2023-12-22 11:35:04 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSMDRa_wmA0mmCdtfDV2PC4jGgt6tgEdg",
+  authDomain: "contador-de-historias-f31c6.firebaseapp.com",
+  databaseURL:
+    "https://contador-de-historias-f31c6-default-rtdb.firebaseio.com",
+  projectId: "contador-de-historias-f31c6",
+  storageBucket: "contador-de-historias-f31c6.appspot.com",
+  messagingSenderId: "597304298997",
+  appId: "1:597304298997:web:cc6be6c3192a53328664bc",
+};
